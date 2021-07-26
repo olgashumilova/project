a69d722f682cfba1bf7d4d9676b682e82f68d1a9
olgashumilova <volga_shumilova@mail.ru>
2021-07-26 17:52:45 +0300

    feat: add search query (in pogress)

--- src/components/searchBar/searchBar.tsx
+++ src/components/searchBar/searchBar.tsx
@@ -3,42 +3,46 @@ import axios from 'axios'
 
 const SearchBar: React.FunctionComponent = () => {
 
-    const [text, setText] = useState('');
-    const [result, setResult] = useState('');
-
-    const [isLoading, setIsLoading] = useState(false)  
-
+  const [text, setText] = useState('');
+  const [result, setResult] = useState('');
+  const [isLoading, setIsLoading] = useState(false) 
     
-      setTimeout(() => {
-        if (text.length !== 0) {
-          setResult(text)
-        } else if (text.length === 0) {
-          setResult('')
-        }
-      }, 300)
-
-      setTimeout(() => {
-        if (result !== text) {
-          setIsLoading(true)
-        } else if (result === text) {
-          setIsLoading(false)
-        }
-      }, 300)
+  setTimeout(() => {
+    if (text.length !== 0) {
+      setResult(text) 
+    } else if (text.length === 0) {
+      setResult('')
+    }
+  }, 300)
+  
+  setTimeout(() => {
+    if (result !== text) {
+      setIsLoading(true)
+    } else if (result === text) {
+      setIsLoading(false)         
+    }
+  }, 300)
       
-    return (
+  return (
+    <div className = 'searchbar'>
+      
+      <input 
+          className = 'searchbar__input'
+          onChange = { 
+            async(event) => {
+              setText(event.target.value)
+                // console.log(event.target.value);   
+              const response = await axios.get(`http://localhost:3001/search/:text`)
+                console.log(response.data)           
+            }
+          }
+          type = 'text'  
+          placeholder = 'Search'
+      />
 
-      <div className = 'searchbar'>
-        
-        <input 
-            className = 'searchbar__input'
-            onChange = {(e) => { setText(e.target.value) }}
-            type = 'text'  
-            placeholder = 'Search'
-        />
-
-        <div className = {isLoading ? 'loader' : ''}>{isLoading}</div>
-      </div>
-    );
+      <div className = {isLoading ? 'loader' : ''}>{isLoading}</div>
+    </div>
+  );
 }
 
 export default SearchBar
