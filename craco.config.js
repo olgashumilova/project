f362dc3742b8c0ae99cea6748f4106f893d0a322
olgashumilova <volga_shumilova@mail.ru>
2021-07-28 17:19:48 +0300

    feat: add paths in craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -6,10 +6,36 @@ module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
-      '@Components': path.resolve(__dirname, 'src/components'),
+
+      //Images
       '@GameCompaniesLogos': path.resolve(__dirname, 'src/assets/gameCompaniesLogos'),
       '@GamePlatformsLogos': path.resolve(__dirname, 'src/assets/gamePlatformsLogos'),
       '@GamesImages': path.resolve(__dirname, 'src/assets/gamesImages'),
-    }
+
+      //Routes
+      '@Routes': path.resolve(__dirname, 'src/components'),
+
+      //Scss
+      '@MainScss': path.resolve(__dirname, 'src/styles'),
+
+      // Search Bar
+      '@SearchBar': path.resolve(__dirname, 'src/components/searchBar'),
+
+      // Game Card
+      '@GameCard': path.resolve(__dirname, 'src/components/gameCard'),
+
+      // Home
+      '@HomeComponent': path.resolve(__dirname, 'src/components/home'), 
+
+      // Footer
+      '@FooterComponent': path.resolve(__dirname, 'src/components/footer'),
+      
+      // Header
+      '@HeaderComponent': path.resolve(__dirname, 'src/components/header'),
+
+      // Modals
+      '@Modal': path.resolve(__dirname, 'src/components/modals'), 
+    },
+    extensions: ["", ".ts", ".tsx", ".js"]
   },
 };
