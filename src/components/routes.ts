1c42ce4dba4fb85793250ad5d94d5db1c11c16af
olgashumilova <volga_shumilova@mail.ru>
2021-07-30 17:13:19 +0300

    feat: add routes

--- src/components/routes.ts
+++ src/components/routes.ts
@@ -3,7 +3,9 @@ const ROUTES = {
     PRODUCTS: '/products',
     ABOUT: '/about',
     SIGNIN: '/sign-in',
-    SIGNUP: '/sign-up'
+    SIGNUP: '/sign-up',
+    USER: '/user',
+    CART: '/cart',
 }
 
 export default ROUTES
