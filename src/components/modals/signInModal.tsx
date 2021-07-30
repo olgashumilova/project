cb308ca48e364e5968f354048ac01baf516117a9
olgashumilova <volga_shumilova@mail.ru>
2021-07-30 17:28:49 +0300

    feat: add functionality to sign in modal window

--- src/components/modals/signInModal.tsx
+++ src/components/modals/signInModal.tsx
@@ -1,26 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import Swal from 'sweetalert2'
+import { Redirect } from 'react-router';
+import { Link } from 'react-router-dom'
+import ROUTES from '@Components/routes.ts'
 
 const SignInModal: React.FunctionComponent = () => {
+
+    const [login, setLogin] = useState('')
+    const [password, setPassword] = useState('')
+    const [usersArray, setUsersArray] = useState(null)
+    const [redirect, setRedirect] = useState(false)
+
+    async function signIn() {
+        const response = await axios.post('http://localhost:3001/signin', 
+        {login, password})
+        Swal.fire(response.data)
+        setRedirect(true)
+        
+        // const usersArray = await axios.get('http://localhost:3001/getUsersArray')
+        // setUsersArray(usersArray)
+        // console.log(usersArray);
+        
+    }
+
     return (
         <div className = 'modalwindow'>
             <div className = 'modalwindow__header'>
                 <p>Authorization</p>
-                <button className = 'modalwindow__close-button'>x</button>
+                <Link to = {ROUTES.HOME}>
+                    <button className = 'modalwindow__close-button'>x</button>
+                </Link>
             </div>
 
             <div className = 'modalwindow__form'>
                 <div className = 'modalwindow__input'>
                     <p>Login</p>
-                    <input type="text" className = 'modalwindow__input-field' />
+                    <input 
+                        type="text" 
+                        className = 'modalwindow__input-field'
+                        onChange = {(event) => setLogin(event.target.value)} 
+                    />
                 </div>
         
                 <div className = 'modalwindow__input'>
                     <p>Password</p>
-                    <input type="text" className = 'modalwindow__input-field' />
+                    <input 
+                        type = 'password'
+                        className = 'modalwindow__input-field'
+                        onChange = {(event) => setPassword(event.target.value)}
+                    />
                 </div>
             </div>
 
-            <button  className = 'modalwindow__button'>Submit</button>
+            <button 
+                className = 'modalwindow__button'
+                type = 'submit'
+                onClick = {() => { signIn() }}>Submit</button>
         </div>
     )
 }
