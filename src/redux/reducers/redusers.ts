63af7d89dcf164d070c03dfb212b664f7fd22b2c
olgashumilova <volga_shumilova@mail.ru>
2021-08-03 17:39:18 +0300

    feat: add reducers

---
+++ src/redux/reducers/redusers.ts
@@ -0,0 +1,35 @@
+import { ACTION_TYPES } from '../const/actionTypes';
+
+export const initialState = {
+    users: [],
+    isSignedIn: false,
+    authUser: null,
+}
+
+const reducer = (state = initialState, action: any) => {
+    switch (action.type) {
+    
+        case ACTION_TYPES.GET_USERS: {
+          return {
+            ...state,
+            users: [...state.users, action.payload],
+          }
+        }
+        case ACTION_TYPES.SET_USER: {
+            return {
+              ...state,
+              authUser: action.payload,
+              isSignedIn: true,
+            }
+        }
+        case ACTION_TYPES.DELETE_USER: {
+            return {
+              ...state,
+              authUser: null,
+              isSignedIn: false,
+            }
+        }
+    }
+}
+
+export default reducer
