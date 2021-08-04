56c071772e4e6d1e1afb16f74db62dbd40baccb2
olgashumilova <volga_shumilova@mail.ru>
2021-08-04 17:42:03 +0300

    feat: add actions

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,5 +1,7 @@
+import axios from 'axios';
 import { ACTION_TYPES } from '../const/actionTypes';
 
+// Users
 export const getUsersArray = (users) => ({
     type: ACTION_TYPES.GET_USERS,
     payload: users,
@@ -7,10 +9,22 @@ export const getUsersArray = (users) => ({
 
 export const setUser = (payload) => ({
     type: ACTION_TYPES.SET_USER,
-    payload: {...payload}
+    payload: payload,
 })
 
-// export const deleteUser = (payload) => ({
-//     type: ACTION_TYPES.SET_USER,
-//     payload: {...payload}
-// })
+export const deleteUser = (payload) => ({
+    type: ACTION_TYPES.DELETE_USER,
+    payload: payload,
+})
+
+// Products
+export const getProducts = () => {
+    return (dispatch) => {
+        axios.get('http://localhost:3001/getProducts').then((response) => {
+          dispatch({
+            type: ACTION_TYPES.GET_PRODUCTS,
+            payload: response.data,
+          })
+        })
+    }
+}
