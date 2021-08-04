fc1328956ccbddfea74617d1ad085f5851b3611a
olgashumilova <volga_shumilova@mail.ru>
2021-08-04 17:57:38 +0300

    feat: add compose to index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import App from '@/App.tsx';
 import { Provider } from 'react-redux';
 import reportWebVitals from '@/reportWebVitals';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import reducer from '../src/redux/reducers/redusers.ts'
 
 ReactDOM.render(
-  <Provider store = {createStore(reducer,applyMiddleware(thunk))}>
+  <Provider store = {createStore(
+    reducer,
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  )}>
     <App />
   </Provider>,
   document.getElementById('root')
