10c16b06419d54ab6220aa8d0dc6273acaa8a7be
olgashumilova <volga_shumilova@mail.ru>
2021-08-04 17:41:22 +0300

    feat: add reducer

--- src/redux/reducers/redusers.ts
+++ src/redux/reducers/redusers.ts
@@ -2,33 +2,40 @@ import { ACTION_TYPES } from '../const/actionTypes';
 
 export const initialState = {
   users: [],
-  isSignedIn: false,
+  products: [],
   authUser: null,
 }
+console.log(initialState.authUser);
 
 const reducer = (state = initialState, action: any) => {
     switch (action.type) {
     
-        case ACTION_TYPES.GET_USERS: {
+      case ACTION_TYPES.GET_USERS: {
+        return {
+          ...state,
+          users: [...state.users, action.payload],
+        }
+      }
+      case ACTION_TYPES.SET_USER: {
+        return {
+          ...state,
+          authUser: action.payload,
+        }     
+      }
+      case ACTION_TYPES.DELETE_USER: {
           return {
             ...state,
-            users: [...state.users, action.payload],
+            authUser: null,
+            isSignedIn: false,
           }
+      }
+      case ACTION_TYPES.GET_PRODUCTS: {
+        return {
+          ...state,
+          products: action.payload,
         }
-        case ACTION_TYPES.SET_USER: {
-          return {
-            ...state,
-            authUser: action.payload,
-            isSignedIn: true,
-          }
-        }
-        case ACTION_TYPES.DELETE_USER: {
-            return {
-              ...state,
-              authUser: null,
-              isSignedIn: false,
-            }
-        }
+      }
+      default: return state
     }
 }
 
