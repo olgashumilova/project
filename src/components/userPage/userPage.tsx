5d7f58294e43aed50a7d6ec2b159c2d3a5eb8e77
olgashumilova <volga_shumilova@mail.ru>
2021-08-06 16:12:05 +0300

    feat: add user page

--- src/components/userPage/userPage.tsx
+++ src/components/userPage/userPage.tsx
@@ -1,53 +1,50 @@
-import React, { useState } from 'react'
-import ChangePassword from '../modals/changePassword'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+import ROUTES from '@Components/routes.ts'
 
 const UserPage: React.FunctionComponent = () => {
 
-    const [showModal, setShowModal] = useState(false)
+    const [userProfile, setUserProfile] = useState([])
 
-    const displayLogin = () => {
-        if (localStorage.getItem('username')) {
-            return localStorage.getItem('username')
-        } else if (localStorage.getItem('username') === null) {
-            return 'User'
+    useEffect(() => {
+    
+      fetchData();
+
+    }, [])
+
+    async function fetchData() {
+        try {
+            await axios.get(`http://localhost:3001/getProfile`).then((response) => {
+                const responseProfile = response.data
+
+                if (responseProfile.length !== 0) {
+                    setUserProfile(response.data)
+                } else if (responseProfile.length === 0) {
+                    setUserProfile([])
+                }      
+            })
+        } catch (error) {
+            console.log(error)
         }
     }
 
-    function modalRenderer() {
-        if (showModal) {
-            return <ChangePassword />
-        }    
-    }
-
     return (
-        <div className = 'userpage'>
-            
-        <div className = 'modalportal'>{modalRenderer()}</div>
+        <div className = 'user-profile-container' id = 'box'>
 
-            <p className = 'userpage__title'>{displayLogin()}&#39;s profile page</p>
+            {userProfile.map((user) => {         
+                return (
+                  <div className = 'user-profile' key = {user.id}>
+                    <p className = 'user-profile__title'>{user.login}&#39;s profile page</p>
+                    <p className = 'user-profile__description'>{user.description}</p>
+                    <img className = 'user-profile__image' src = {user.userImage}></img>
+                  </div>
+                )
+            })}
 
-            <div className = 'userpage__main'>
-
-                <div className = 'userpage__userpic-container'>
-                    <div className = 'userpage__image'>
-                        <div className = 'userpage__image-inner'>No picture</div>
-                    </div>
-                    <button className = 'userpage__button'>Change profile picture</button>
-                </div>
-
-                <div className = 'userpage__profile-desc'>
-                    <p>User Name</p>
-                    <input type = 'text' className = 'userpage__input'></input>
-
-                    <p>Profile description</p>
-                    <textarea className = 'userpage__input userpage__input-description'></textarea>
-                </div>
-
-                <div className = 'userpage__buttons'>
-                    <button className = 'userpage__button'>Save profile</button>
-                    <button className = 'userpage__button' onClick = {() => setShowModal(!showModal)}>Change password</button>
-                </div>
-            </div>
+            <Link to = {ROUTES.EDIT_USER}>
+                <button className = 'user-profile__button'>Edit profile</button>
+            </Link>
         </div>
     )
 }
