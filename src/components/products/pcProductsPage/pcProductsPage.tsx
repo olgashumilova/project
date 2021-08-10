48dda8b6b54276e07d3db9d8660ce59020a02495
olgashumilova <volga_shumilova@mail.ru>
2021-08-10 17:47:57 +0300

    feat: add platform pages content

--- src/components/products/pcProductsPage/pcProductsPage.tsx
+++ src/components/products/pcProductsPage/pcProductsPage.tsx
@@ -1,11 +1,84 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux';
+import { getProductsAPI } from '@/api/api'
+import SearchProductsPage from '@Components/products/searchProductsPage.tsx'
 
-import SearchProductsPage from '../searchProductsPage'
+// Game Card
+import GameCard from '@Components/gameCard/gameCard.tsx'
+import '@Components/gameCard/gameCard.scss'
 
 const PcProductsPage: React.FunctionComponent = () => {
+
+    const filteredProducts = useSelector(state => state.filteredProducts)
+
+    const [productsArray, setProductsArray] = useState([])
+    const [pcProducts, setPcProducts] = useState([])
+
+    async function getProducts() {
+        try {
+            await getProductsAPI.then((response) => {
+                setProductsArray(response.data)
+            })        
+        } catch (error) {
+            console.log(error);         
+        }   
+    }
+
+    useEffect(() => {
+        getProducts()
+    }, [])
+
+    const filterPcGames = () => {
+        const newArr = productsArray.filter((game) => game.platform.pc )
+        setPcProducts(newArr)
+        console.log(newArr);
+    }
+    
+    useEffect(() => {
+        filterPcGames()
+    }, [productsArray])
+
+    console.log(filteredProducts.find(item => item.platform.pc));
+
+    function func() {
+        if (filteredProducts.length === 0) {
+            return (
+                pcProducts.map((game) => {
+                    return (
+                        <div key = {game.index}>
+                            <GameCard className = 'catalog-gamecard' 
+                                backgroundImage = {game.image} 
+                                description = {game.description}
+                                ageLimit = {game.ageLimit}
+                                price = {`Price: ${game.price}$`}
+                            />
+                        </div>
+                    )
+                })
+            )
+        } else if (filteredProducts.length !== 0 && filteredProducts.find(item => item.platform.pc)){
+            return (
+                filteredProducts.map((game) => {
+                    return (
+                        <div key = {game.index}>
+                            <GameCard className = 'catalog-gamecard' 
+                                backgroundImage = {game.image} 
+                                description = {game.description}
+                                ageLimit = {game.ageLimit}
+                                price = {`Price: ${game.price}$`}
+                            />
+                        </div>
+                    )
+                })
+            )
+        }
+    }
+                        
     return (
-        <SearchProductsPage title = 'PC'/>
-    )
+        <div>
+            <SearchProductsPage title = 'PC' filterByPlatform = {func()}/>
+        </div>
+    ) 
 }
 
 export default PcProductsPage
--- src/components/products/playstationProductsPage/playstationProductsPage.tsx
+++ src/components/products/playstationProductsPage/playstationProductsPage.tsx
@@ -1,10 +1,82 @@
-import React from 'react'
-import SearchProductsPage from '../searchProductsPage'
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux';
+import { getProductsAPI } from '@/api/api'
+import SearchProductsPage from '@Components/products/searchProductsPage.tsx'
+
+// Game Card
+import GameCard from '@Components/gameCard/gameCard.tsx'
+import '@Components/gameCard/gameCard.scss'
 
 const PlaystationProductsPage: React.FunctionComponent = () => {
+
+    const filteredProducts = useSelector(state => state.filteredProducts)
+
+    const [productsArray, setProductsArray] = useState([])
+    const [playstationProducts, setPlaystationProducts] = useState([])
+
+    async function getProducts() {
+        try {
+            await getProductsAPI.then((response) => {
+                setProductsArray(response.data)
+            })        
+        } catch (error) {
+            console.log(error);         
+        }   
+    }
+
+    useEffect(() => {
+        getProducts()
+    }, [])
+
+    const filterPlaystationGames = () => {
+        const newArr = productsArray.filter((game) => game.platform.playstation )
+        setPlaystationProducts(newArr)
+        console.log(newArr);
+    }
+    
+    useEffect(() => {
+        filterPlaystationGames()
+    }, [productsArray])
+
+    console.log(filteredProducts.find(item => item.platform.playstation));
+
+    function func() {
+        if (filteredProducts.length === 0) {
+            return (
+                playstationProducts.map((game) => {
+                    return (
+                        <div key = {game.index}>
+                            <GameCard className = 'catalog-gamecard' 
+                                backgroundImage = {game.image} 
+                                description = {game.description}
+                                ageLimit = {game.ageLimit}
+                                price = {`Price: ${game.price}$`}
+                            />
+                        </div>
+                    )
+                })
+            )
+        } else if (filteredProducts.length !== 0 && filteredProducts.find(item => item.platform.playstation)){
+            return (
+                filteredProducts.map((game) => {
+                    return (
+                        <div key = {game.index}>
+                            <GameCard className = 'catalog-gamecard' 
+                                backgroundImage = {game.image} 
+                                description = {game.description}
+                                ageLimit = {game.ageLimit}
+                                price = {`Price: ${game.price}$`}
+                            />
+                        </div>
+                    )
+                })
+            )
+        }
+    }
+                        
     return (
         <div>
-            <SearchProductsPage title = 'PlayStation'/>
+            <SearchProductsPage title = 'Playstation' filterByPlatform = {func()}/>
         </div>
     )
 }
