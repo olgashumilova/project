c96dc39b0df580ebd68d4eea7952cb9f854734d2
olgashumilova <volga_shumilova@mail.ru>
2021-08-10 17:49:54 +0300

    feat: add sorting and filters

--- src/components/products/searchProductsPage.tsx
+++ src/components/products/searchProductsPage.tsx
@@ -1,30 +1,27 @@
 import React, { useEffect, useState } from 'react'
-import { string } from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import 'semantic-ui-css/semantic.min.css'
+import { Dropdown } from 'semantic-ui-react'
 
 // Search Bar Component
 import SearchBar from '@Components/searchBar/searchBar.tsx'
 import '@Components/searchBar/searchBar.scss'
 
-// Game Card
-import GameCard from '@Components/gameCard/gameCard.tsx'
-import '@Components/gameCard/gameCard.scss'
-
-import { getProductsAPI } from '../../api/api';
+import { getProductsAPI } from '@/api/api';
+import { getFilteredProducts } from '@/redux/actions/actions'
 
 const SearchProductsPage: React.FunctionComponent = (props) => {
 
-    const [productsArray, setProductsArray] = useState([])
+    const dispatch = useDispatch()
     
-
     SearchProductsPage.propTypes = {
-        title: string,
+        title: PropTypes.string,
+        filterByPlatform: PropTypes.array,
     }
 
-    useEffect(() => {
-
-        getProducts()
-
-    }, [])
+    const [productsArray, setProductsArray] = useState([])
 
     async function getProducts() {
         try {
@@ -36,13 +33,67 @@ const SearchProductsPage: React.FunctionComponent = (props) => {
         }   
     }
 
-    const filter = () => {
-        for (let i = 0; i < productsArray.length; i++) {
-            const newArr = productsArray.filter((game) => game.platform.pc )
-            console.log(newArr)
+    useEffect(() => {
+        getProducts()
+    }, [])
+
+    // Filters____________________________________________________________________________________________
+
+    const filterByGenre = (value) => {
+        if (value === 'All genres') {
+            dispatch(getFilteredProducts(productsArray.filter((game) => game.genre)))
+        } else if (value === 'Shooter') {
+            dispatch(getFilteredProducts(productsArray.filter((game) => game.genre == 'Shooter')))
+        } else if (value === 'Sandbox') {
+            dispatch(getFilteredProducts(productsArray.filter((game) => game.genre == 'Sandbox')))
+        } else if (value === 'RPG') {
+            dispatch(getFilteredProducts(productsArray.filter((game) => game.genre == 'RPG')))
+        } else if (value === 'Simulator') {
+            dispatch(getFilteredProducts(productsArray.filter((game) => game.genre == 'Simulator')))
+        } else if (value === 'Action-adventure') {
+            dispatch(getFilteredProducts(productsArray.filter((game) => game.genre == 'Action-adventure')))
         }
     }
-    filter()
+
+    const filterByAge = (value) => {
+        if (value === 'All ages') {
+            dispatch(getFilteredProducts(productsArray.filter((game) => game.ageLimit)))
+        } else if (value === '3 +') {
+            dispatch(getFilteredProducts(productsArray.filter((game) => (game.ageLimit == '3 +'))))
+        } else if (value === '6 +') {
+            dispatch(getFilteredProducts(productsArray.filter((game) => game.ageLimit == '6 +')))
+        } else if (value === '12 +') {
+            dispatch(getFilteredProducts(productsArray.filter((game) => game.ageLimit == '12 +')))
+        } else if (value === '16 +') {
+            dispatch(getFilteredProducts(productsArray.filter((game) => game.ageLimit == '16 +')))
+        } else if (value === '18 +') {
+            dispatch(getFilteredProducts(productsArray.filter((game) => game.ageLimit == '18 +')))
+        }
+    }
+
+    const sortByName = () => {
+        const result = productsArray.sort(function(a, b) {
+            const nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase()
+            if (nameA < nameB)
+              return -1
+            if (nameA > nameB)
+              return 1
+            return 0
+        })
+        return dispatch(getFilteredProducts(result))      
+    }
+
+    const sortByRating = () => {
+        const result = productsArray.sort(function(a, b) {
+            const ratingA = a.rating, ratingB = b.rating
+            if (ratingA < ratingB)
+              return -1
+            if (ratingA > ratingB)
+              return 1
+            return 0
+        })
+        return dispatch(getFilteredProducts(result))      
+    }
 
     return (
         <div className = 'products-page'>
@@ -53,17 +104,25 @@ const SearchProductsPage: React.FunctionComponent = (props) => {
 
                 <div className = 'products-page__container'>
                     <p>Criteria</p>
-                    <select>
-                        <option>Name</option>
-                        <option>Rating</option>
-                        <option>Price</option>
-                        <option>Age Limit</option>
-                    </select>
+                    <Dropdown text = 'Select'>
+                        <Dropdown.Menu className = 'products-page__select'>
+                            <Dropdown.Item text = 'Name' onClick = {() => sortByName()}/>
+                            <Dropdown.Item text = 'Rating' onClick = {() => sortByRating()}/>
+                            <Dropdown.Item text = 'Price'/>
+                            <Dropdown.Item text = 'Age Limit'/>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    {/* <select className = 'products-page__select' onChange = {() => sortByName()}>
+                        <option value = '1' >Name</option>
+                        <option value = '2' >Rating</option>
+                        <option value = '3' >Price</option>
+                        <option value = '4' >Age Limit</option>
+                    </select> */}
                 </div>
                 
                 <div className = 'products-page__container'>
                     <p>Type</p>
-                    <select>
+                    <select className = 'products-page__select'>
                         <option>Ascending</option>
                         <option>Descending</option>
                     </select>
@@ -73,23 +132,33 @@ const SearchProductsPage: React.FunctionComponent = (props) => {
                     <p className = 'products-page__title'>Genres</p>
                     <ul className = 'products-page__selection-list'>
                         <li>
-                            <input type = 'checkbox'></input>
+                            <input name = 'genreFilter' type = 'radio' onChange = {() => filterByGenre('All genres')}></input>
                             <span className = 'products-page__selectors'>All genres</span>
                         </li>
 
                         <li>
-                            <input type = 'checkbox'></input>
+                            <input name = 'genreFilter' type = 'radio' onChange = {() => filterByGenre('Shooter')}></input>
                             <span className = 'products-page__selectors'>Shooter</span> 
                         </li>
 
                         <li>
-                            <input type = 'checkbox'></input>
-                            <span className = 'products-page__selectors'>Arcade</span>
+                            <input name = 'genreFilter' type = 'radio' onChange = {() => filterByGenre('Sandbox')}></input>
+                            <span className = 'products-page__selectors'>Sandbox</span>
                         </li>
 
                         <li>
-                            <input type = 'checkbox'></input>
-                            <span className = 'products-page__selectors'>Survive </span>                      
+                            <input name = 'genreFilter' type = 'radio' onChange = {() => filterByGenre('RPG')}></input>
+                            <span className = 'products-page__selectors'>RPG</span>                      
+                        </li>
+
+                        <li>
+                            <input name = 'genreFilter' type = 'radio' onChange = {() => filterByGenre('Simulator')}></input>
+                            <span className = 'products-page__selectors'>Simulator</span>                      
+                        </li>
+
+                        <li>
+                            <input name = 'genreFilter' type = 'radio' onChange = {() => filterByGenre('Action-adventure')}></input>
+                            <span className = 'products-page__selectors'>Action-adventure</span>
                         </li>
                     </ul>
                 </div>
@@ -98,28 +167,33 @@ const SearchProductsPage: React.FunctionComponent = (props) => {
                     <p className = 'products-page__title'>Age</p>
                     <ul className = 'products-page__selection-list'>
                         <li>
-                            <input type = 'checkbox'></input>
+                            <input name = 'ageFilter' type = 'radio' onChange = {() => filterByAge('All ages')}></input>
                             <span className = 'products-page__selectors'>All ages</span>
                         </li>
 
                         <li>
-                            <input type = 'checkbox'></input>
-                            <span className = 'products-page__selectors'>3+</span> 
+                            <input name = 'ageFilter' type = 'radio' onChange = {() => filterByAge('3 +')}></input>
+                            <span className = 'products-page__selectors'>3 +</span> 
                         </li>
 
                         <li>
-                            <input type = 'checkbox'></input>
-                            <span className = 'products-page__selectors'>6+</span>
+                            <input name = 'ageFilter' type = 'radio' onChange = {() => filterByAge('6 +')}></input>
+                            <span className = 'products-page__selectors'>6 +</span>
                         </li>
 
                         <li>
-                            <input type = 'checkbox'></input>
-                            <span className = 'products-page__selectors'>12+ </span>                      
+                            <input name = 'ageFilter' type = 'radio' onChange = {() => filterByAge('12 +')}></input>
+                            <span className = 'products-page__selectors'>12 +</span>                      
                         </li>
 
                         <li>
-                            <input type = 'checkbox'></input>
-                            <span className = 'products-page__selectors'>18+</span>                      
+                            <input name = 'ageFilter' type = 'radio' onChange = {() => filterByAge('16 +')}></input>
+                            <span className = 'products-page__selectors'>16 + </span>                      
+                        </li>
+
+                        <li>
+                            <input name = 'ageFilter' type = 'radio' onChange = {() => filterByAge('18 +')}></input>
+                            <span className = 'products-page__selectors'>18 +</span>                      
                         </li>
 
                     </ul>
@@ -130,18 +204,10 @@ const SearchProductsPage: React.FunctionComponent = (props) => {
 
             <div className = 'products-page__search'>
                 <SearchBar />
+                <p className = 'products-page__products-title'>Products</p>
 
                 <div className = 'catalog'>
-                    {productsArray.map((game) => {
-                        
-                        return (
-                            <GameCard className = 'catalog-gamecard' key = {game.index} 
-                                backgroundImage = {game.image} 
-                                description = {game.description}
-                                ageLimit = {game.ageLimit} 
-                            />
-                        )
-                    })}
+                    {props.filterByPlatform}
                 </div>
             </div>
             
