e4ea58e20cfd23fd1a41c7057cf71de36abf7042
olgashumilova <volga_shumilova@mail.ru>
2021-08-12 17:32:53 +0300

    feat: add cart page

--- src/components/cart/cartPage.tsx
+++ src/components/cart/cartPage.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState, useEffect, useMemo } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import Swal from 'sweetalert2'
+import axios from 'axios'
+import { sendOrderToServerAPI } from '@/api/api'
+import { removeItemFromCart } from '@/redux/actions/actions'
 
 const CartPage: React.FunctionComponent = () => {
 
+    const dispatch = useDispatch()
+
     const cart = useSelector(state => state.cart)
+    let totalPrice = 0
+
+    const [itemCount, setItemCount] = useState(1);
 
     let today = new Date();
     const dd = String(today.getDate()).padStart(2, '0');
-    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const mm = String(today.getMonth() + 1).padStart(2, '0')
     const yyyy = today.getFullYear();
 
     today = mm + ' / ' + dd + ' / ' + yyyy;
@@ -15,7 +24,7 @@ const CartPage: React.FunctionComponent = () => {
     if (cart.length === 0) {
         return (
             <div className = 'cart__empty'>
-                <p>Cart is empty </p>
+                <p>Cart is empty</p>
                 <div className = 'empty-cart'></div>
             </div>
         )
@@ -26,16 +35,50 @@ const CartPage: React.FunctionComponent = () => {
                 <p className = 'cart__title' >Cart page</p>
                 
                 <div className = 'cart__main'>
-                    {cart.map((game) => {
-                        return (
-                            <div key = {game.index}>
-                                <p>{game.name}</p>
-                                <p>{today}</p>
-                            </div>
-                        )
-                        
-                    })}
+                    <p>Name</p>
+                    <p>Platform</p>
+                    <p>Order date</p>
+                    <p>Amount</p>
+                    <p>Price ($)</p>
                 </div>
+
+                {cart.map((game, index) => {
+                    totalPrice += game.price * game.amount
+                    
+                    return (
+                        <div className = 'cart__products' key = {index}>
+                        
+                            <div className = 'game-border'> <p>{game.name}</p> </div>
+                            <div className = 'game-border'>
+                                <select>
+                                    <option>PC</option>
+                                    <option>Playstation</option>
+                                    <option>Xbox</option>
+                                </select>
+                            </div>
+                            <div className = 'game-border'> <p>{today}</p> </div>
+                            <div className = 'game-border'>
+                            <div className = 'cart__change-quantity-btn'>
+                                <button className = 'cart__change-quantity-btn' onClick = {() => {
+                                    game.amount <= 0 ? game.amount = 1 : setItemCount(--game.amount)
+                                }}> - </button>
+                                <p className = 'cart__current-item-amount'>{game.amount}</p>
+                                <button className = 'cart__change-quantity-btn' onClick = {() => {
+                                    setItemCount(++game.amount)
+                                }}> + </button>
+                            </div>
+                            </div>
+                            <div className = 'game-border'> <p>{game.price * game.amount}$</p> </div>
+                            <div className = 'cart__game-button' onClick = {() => dispatch(removeItemFromCart(index))}><button>x</button></div> 
+                        </div>
+                    )
+                })}
+                <p className = 'cart__total'>Games cost: {totalPrice}$</p>
+                <button className = 'cart__purchase-button' onClick = {
+                    async () => {
+                        const response = await axios.post(sendOrderToServerAPI, {cart})
+                        Swal.fire(response.data);
+                }}>Buy</button>
             </div>
         )
     }
