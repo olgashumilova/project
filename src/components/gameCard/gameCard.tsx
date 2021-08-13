5c83cdbc923fa87c8c4bcc1b09f39b59e8edc9f8
olgashumilova <volga_shumilova@mail.ru>
2021-08-13 17:25:26 +0300

    feat: add admin edit card button

--- src/components/gameCard/gameCard.tsx
+++ src/components/gameCard/gameCard.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import Swal from 'sweetalert2'
 import { useDispatch } from 'react-redux'
-import { addItemToCart } from '../../redux/actions/actions'
+import { addItemToCart, getCurrentGameCard } from '@/redux/actions/actions'
 import { getProductsAPI } from '@/api/api'
+import EditGameCardModal from '@Components/modals/editGameCardModal'
 
 interface IProp {
     backgroundImage: string;
@@ -15,7 +16,9 @@ const GameCard: React.FunctionComponent <{backgroundImage: string, description: 
     
     const dispatch = useDispatch()
 
+    const [showModal, setShowModal] = useState(false)
     const [productsArray, setProductsArray] = useState([])
+
     async function getProducts() {
         try {
             await getProductsAPI.then((response) => {
@@ -38,19 +41,58 @@ const GameCard: React.FunctionComponent <{backgroundImage: string, description: 
         }
     }
 
-    return (
-        <div className = 'gamecard gamecard-wrap'>
-            <div className = 'front' style = {{backgroundImage: `url(${backgroundImage})`}}></div>
-            <div className = 'back'>
-                <p className = 'back__description'>{description}</p>
-                <p>{ageLimit}</p>
-                <p>{price}</p>
-                <button className = 'back__cart-button' onClick = {() => {
-                    dispatchItem()
-                    Swal.fire(`Added to cart`)
-                }}>Add to cart</button></div>
-        </div>
-    )
+    function showModalAndDispatch() {
+        for (let i = 0; i < productsArray.length; i++) {
+            if (productsArray[i].description === description) {
+                dispatch(getCurrentGameCard(productsArray[i]))
+                setShowModal(!showModal)
+            }  
+        }
+    }
+
+    function modalRenderer() {
+        if (showModal) {
+            return <EditGameCardModal />
+        } else {
+            null
+        }  
+    }
+
+    const userName = localStorage.getItem('username')
+    
+    if (userName === 'admin') {
+        return (
+            <div className = 'modalportal'>{modalRenderer()}
+                <div className = 'gamecard gamecard-wrap'>
+                <div className = 'front' style = {{backgroundImage: `url(${backgroundImage})`}}></div>
+                <div className = 'back'>
+                    <p className = 'back__description'>{description}</p>
+                    <p>{ageLimit}</p>
+                    <p>{price}</p>
+                    <button className = 'back__card-button' onClick = {() => {
+                        dispatchItem()
+                        Swal.fire(`Added to cart`)
+                    }}>Add to cart</button>
+                    <button className = 'back__card-button edit-card-button' onClick = {() => showModalAndDispatch()}>Edit</button>
+                </div>
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <div className = 'gamecard gamecard-wrap'>
+                <div className = 'front' style = {{backgroundImage: `url(${backgroundImage})`}}></div>
+                <div className = 'back'>
+                    <p className = 'back__description'>{description}</p>
+                    <p>{ageLimit}</p>
+                    <p>{price}</p>
+                    <button className = 'back__card-button' onClick = {() => {
+                        dispatchItem()
+                        Swal.fire(`Added to cart`)
+                    }}>Add to cart</button></div>
+            </div>
+        )
+    }
 }
 
 export default GameCard
