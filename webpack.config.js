e1dd32a04804064ae428036d6c0ef2609501db83
olgashumilova <volga_shumilova@mail.ru>
2021-08-25 13:11:11 +0300

    refactor: delete file

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
-module.exports = {
-  plugins: [
-    new BundleAnalyzerPlugin()
-  ]
-}
